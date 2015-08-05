8bd587decd81f3ba1be29b6c99b777d642f5b275
EJLarcs <ejlarcombe@gmail.com>
2015-08-05 16:47:23 -0400

    create files

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,13 @@
+require_relative 'song.rb'
+
+
 class Artist
   # code goes here
+  attr_reader :name
+
+  def initialize(name)
+    @name = name 
+  end
+
+#initialize the class Artist with a name .. initializing_the_class(name)
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,44 @@
+require_relative 'artist.rb'  # => false, true
+require 'tempfile'            # => true, false
+require 'pry'
+
 class Song
   # code goes here
+
+  attr_accessor :title, :artist, :serialize  # => nil, nil
+
+  # def initialize
+  #   @title = title
+  # end
+
+  def title
+    @title
+  end
+
+  def artist=(artist)
+    @artist = artist
+  end
+
+  def artist
+    @artist 
+  end
+
+  def serialize
+    #Replaces spaces in the song title with underscores and makes every character lower case.
+    file_title = "#{@title}".tr(' ', '_')
+    # file_title2 = file_title.downcase + ".txt"
+    file = Tempfile.new(["#{file_title.downcase}", ".txt"], "tmp")
+      # f = Tempfile.open('#{file_title2}', 'tmp')
+      #             #    This filename contains 'foo' in its basename.
+    file.write("#{@artist.name} - #{@title}")
+ 
+   
+    file.close
+
+  end
+
+
 end
+
+
+
